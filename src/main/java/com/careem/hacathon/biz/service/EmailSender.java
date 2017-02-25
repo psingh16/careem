44bfda12c2f99d1266641e2de3771d328115f997
ranjan123 <ranjan.kumar@zapcg.com>
2017-02-25 21:57:02 +0530

    email functionality added

---
+++ src/main/java/com/careem/hacathon/biz/service/EmailSender.java
@@ -0,0 +1,46 @@
+package com.careem.hacathon.biz.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+/*
+ * @author Ranjan Kumar
+ */
+@Service
+public class EmailSender {
+
+	
+	@Autowired
+	JavaMailSender javaMailSender;
+	public EmailSender() {
+		
+	}
+	Logger logger=LoggerFactory.getLogger(EmailSender.class);
+	public void sendMailRequestQuote(String from, String to, String subject, String msg) {
+
+		
+		MimeMessage message = javaMailSender.createMimeMessage();
+		try {
+			message.setSubject(subject);
+			MimeMessageHelper helper;
+			helper = new MimeMessageHelper(message, true);
+			helper.setFrom(from);
+			helper.setTo(to);
+			helper.setText(msg, true);
+			logger.info("before sending mail");
+			javaMailSender.send(message);
+			logger.info("Sending mail successfull");
+		} catch (MessagingException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+}
